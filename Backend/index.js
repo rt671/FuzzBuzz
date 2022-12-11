f95375064f1f5a183ce2865f6f1123bace6ac7c5
Rudraksh <rudrakshsuper2001@gmail.com>
2022-12-11 12:00:41 +0530

    Search functionality working

--- Backend/index.js
+++ Backend/index.js
@@ -15,26 +15,26 @@ db.once("open", () => console.log("Connected to Mongoose"));
 
 app.get("/upload", (req, res) => { //POST REQUEST=> INPUT: TEXT, PASSWORD
 
-  // const data = req.body.text;
-  // console.log(data)
-  const data = "America, officially the Republic of India, is a country in South Asia." 
+  const data = "cat bat mat rat sat chat wait"
   const password = "helloworld"
   
   //CLEAN THE TEXT
   const preprocess = spawnSync("python", ["D:/work/BTP/CODE/Main/preprocess.py", data]);
   const cleaned_doc = preprocess.output.toString('utf8');
-
-  //Encrypt the Text
+  console.log(cleaned_doc)
+  
+  //ENCRYPT THE TEXT
   const encryptingDoc = spawnSync("python", ["D:/work/BTP/CODE/Main/encrypt_text.py", cleaned_doc, password]);
   const encr_text = encryptingDoc.output.toString('utf8');
+  console.log(encr_text);
 
-  //Saving the Encrypted Text
+  //SAVING THE ENCRYPTED TEXT
   const newText = new Text({text: encr_text});
   newText
     .save()
     .then((savedText) => {
       const id = savedText._id.toString(); 
-      console.log("encrypted text is saved!", "id is ", id)
+      console.log("Encrypted text is saved!", "id is ", id)
 
       //Creating the Index Table
       const indexing = spawnSync("python", [
@@ -43,13 +43,16 @@ app.get("/upload", (req, res) => { //POST REQUEST=> INPUT: TEXT, PASSWORD
       ]);
       const idx_table = indexing.output.toString('utf8');
 
-      //Encrypting the Index Table
+      // Encrypting the Index Table
       const encrypt = spawnSync("python", [
         "D:/work/BTP/CODE/Main/encrypt_index.py",
         idx_table, password
       ]);
-      const encr_idx = encrypt.output.toString('utf-8')
+      
+      // const jsonData = JSON.parse(encrypt.stdout);
+      // console.log(jsonData)
 
+      const encr_idx = encrypt.output.toString('utf8')
       //Saving the Encrypted Table
       User.findOneAndUpdate({password:password}, {index:encr_idx}, {new:true})
       .catch(err => console.log(err))
@@ -62,7 +65,7 @@ app.get("/upload", (req, res) => { //POST REQUEST=> INPUT: TEXT, PASSWORD
 
 
 app.get('/search', (req, res)=>{ //POST =>  KEYWORD, PASSWORD
-  const keyword = "india";
+  const keyword = "wait";
   const password = "helloworld"
 
   const trapdoor = spawnSync("python", [
@@ -70,20 +73,22 @@ app.get('/search', (req, res)=>{ //POST =>  KEYWORD, PASSWORD
     keyword, password
   ]);
   const trapdoor_set = trapdoor.output.toString('utf-8')
-  
-  // console.log("TRAPDOOR SET IS ", trapdoor_set)
-
+  console.log(trapdoor_set)
   User.find({password:password})
   .then(user => 
     {
         const ind_table = user[0].index;
-        // res.send(ind_table)
-        // console.log("THE TABLE IS ", ind_table);
         const search = spawnSync("python", [
-          "D:/work/BTP/CODE/Main/search.py", trapdoor_set
+          "D:/work/BTP/CODE/Main/search.py", trapdoor_set, ind_table
         ]);
         const search_res = search.output.toString('utf-8')
-        res.send(search_res)
+        console.log(search_res)
+        // res.send(search_res)
+
+        const fetchArray = spawnSync("python", ["D:/work/BTP/CODE/Main/findnsort.py", 7]);
+        const documents = fetchArray.output.toString('utf-8')
+        console.log(documents)
+        res.send(documents)
     })
   .catch(err => res.json(err))
 })
@@ -91,4 +96,3 @@ app.get('/search', (req, res)=>{ //POST =>  KEYWORD, PASSWORD
 app.listen(port, () =>
   console.log(`Example app listening on port ${port}!`)
 );
- 
